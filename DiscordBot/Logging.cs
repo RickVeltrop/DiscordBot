ecbdd0ec43dba0e06bbc5646576720739e85f4cf
RickVeltrop <retardnt.frog@outlook.com>
2023-10-24 13:39:57 +0200

    Basic discord bot setup
    - Basic discord bot setup such as login and logging
    - Added TOKEN to env variables
    - Created logging service

---
+++ DiscordBot/Logging.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot;
+
+public class LoggingService
+{
+    private DiscordSocketClient _client;
+    private CommandService _commands;
+
+    private Task LogAsync(LogMessage Msg)
+    {
+        if (Msg.Exception is CommandException CmdException)
+        {
+            Console.WriteLine($"[Command/{Msg.Severity}] {CmdException.Command.Aliases.First()}"
+                + $" failed to execute in {CmdException.Context.Channel}.");
+            Console.WriteLine(CmdException);
+        }
+        else
+            Console.WriteLine($"[General/{Msg.Severity}] {Msg}");
+
+        return Task.CompletedTask;
+    }
+
+    private Task ReadyAsync()
+    {
+        Console.WriteLine($"Logged in as {_client.CurrentUser}");
+
+        return Task.CompletedTask;
+    }
+
+    public LoggingService(DiscordSocketClient Client, CommandService Cmds)
+    {
+        _client = Client;
+        _commands = Cmds;
+    }
+
+    public Task InitializeAsync()
+    {
+        _client.Log += LogAsync;
+        _client.Ready += ReadyAsync;
+        _commands.Log += LogAsync;
+
+        return Task.CompletedTask;
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,11 +1,46 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot;
 
 internal class Program
 {
-    static void Main(string[] args)
+    private readonly IServiceProvider _serviceProvider;
+    private string? _token = Environment.GetEnvironmentVariable("TOKEN");
+
+    public Program()
     {
-        Console.WriteLine("Hello, World!");
+        _serviceProvider = CreateProvider();
+    }
+
+    private static IServiceProvider CreateProvider()
+    {
+        var Config = new DiscordSocketConfig() { };
+
+        var Collection = new ServiceCollection()
+            .AddSingleton(Config)
+            .AddSingleton<CommandService>()
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton<LoggingService>();
+
+        return Collection.BuildServiceProvider();
+    }
+
+    public static void Main(string[] args) =>
+        new Program().MainAsync(args).GetAwaiter().GetResult();
+
+    public async Task MainAsync(string[] args)
+    {
+        var _client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+        var _logging = _serviceProvider.GetRequiredService<LoggingService>();
+
+        await _logging.InitializeAsync();
+
+        await _client.LoginAsync(TokenType.Bot, _token, true);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
     }
 }
